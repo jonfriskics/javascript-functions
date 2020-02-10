2208fd8de6475c919631bf30393bd512491d8f29
Jon Friskics <jonfriskics@gmail.com>
2020-02-10 14:35:57 -0500

    task 3 answer

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -3,15 +3,19 @@ function seed() {
 }
 
 function same([x, y], [j, k]) {
-  // if(x == j && y == k) {
-  //   return true
-  // }
-  // return false
+  if(x == j && y == k) {
+    return true
+  }
+  return false
 }
 
 // The game state to search for `cell` is passed as the `this` value of the function.
 function contains(cell) {
-
+  if (cell.x == this.contains().x && cell.y == this.contains().y) {
+    return true
+  } else {
+    return false
+  }
 }
 
 const printCell = (cell, state) => {};
