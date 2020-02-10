255c885e34ef019c72f64553a67937cd254f94a8
Jon Friskics <jonfriskics@gmail.com>
2020-02-10 14:26:39 -0500

    task 2 answer

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -2,7 +2,12 @@ function seed() {
   return Array.prototype.slice.call(arguments)
 }
 
-function same([x, y], [j, k]) {}
+function same([x, y], [j, k]) {
+  if(x == j && y == k) {
+    return true
+  }
+  return false
+}
 
 // The game state to search for `cell` is passed as the `this` value of the function.
 function contains(cell) {}
